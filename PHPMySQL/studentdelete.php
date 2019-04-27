c6d80c01c432036103efbc8dd773909b63a73124
MusharibIqbal <45254288+MusharibIqbal@users.noreply.github.com>
2019-04-27 15:18:07 +0500

    Create studentdelete.php

---
+++ PHPMySQL/studentdelete.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Student</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <?php
+    require_once("config.php");
+    $sid = $name = $regno = "";
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        if (!isset($_GET['id'])) {
+            echo "id is not set";
+            return;
+        } else if (empty($_GET['id'])) {
+            echo "id is empty";
+            return;
+        } else {
+            $sid = $_GET['id'];
+            $connection = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+            if (mysqli_connect_error()) {
+                die(mysqli_connect_error());
+            }
+
+            $sql = "DELETE FROM student WHERE id = $sid";
+
+            $result = mysqli_query($connection, $sql);
+        
+            if (mysqli_error($connection)) {
+                die("Something went wrong! Check your Database");
+            }
+            mysqli_close($connection);
+        }
+    }
+    ?>
+</head>
+<body>
+<?php
+include_once ("navigation.html");
+?>
+<div class="container col-md-5">
+    <p>Record is deleted</p>
+</div>
+</body>
+</html>
