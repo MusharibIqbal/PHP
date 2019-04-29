17b9001e23ec40e3edef5c152c6fbc16c77dcb49
MusharibIqbal <45254288+MusharibIqbal@users.noreply.github.com>
2019-04-29 22:05:52 +0500

    Create header.php

---
+++ E-commerce App/header.php
@@ -0,0 +1,59 @@
+ <header>
+        <div id="top-header">
+            <div class="container">
+            </div>
+        </div>
+        <div id="header">
+            <div class="container">
+                <div class="pull-left">
+                    <div class="header-logo">
+                        <a class="logo" href="#">
+                            <img src="./img/logo.jpg"  alt="" height="100" width="20" >
+                        </a>
+                    </div>
+                </div>
+                <div class="pull-right">
+                    <ul class="header-btns">
+                        <li class="nav-toggle">
+                            <button class="nav-toggle-btn main-btn icon-btn"><i class="fa fa-bars"></i></button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="navigation">
+        <div class="container">
+            <div id="responsive-nav">
+                <div class="category-nav show-on-click" >
+                    <span class="category-header">Categories <i class="fa fa-list"></i></span>
+                    <ul class="category-list">
+
+                        <li><a href="#">Fashion</a></li>
+                        <li><a href="#">Electronics</a></li>
+                        <li><a href="#">Sports</a></li>
+                        <li><a href="#">Acessories</a></li>
+                    </ul>
+                </div>
+                <div class="menu-nav">
+                    <span class="menu-header">Menu <i class="fa fa-bars"></i></span>
+                    <ul class="menu-list">
+                    <li><a href="#">Fashion</a></li>
+                        <li><a href="#">Electronics</a></li>
+                        <li><a href="#">Sports</a></li>
+                        <li><a href="#">Acessories</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="breadcrumb">
+        <div class="container">
+            <ul class="breadcrumb">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Category</a></li>
+                <li class="active">Laptop</li>
+            </ul>
+        </div>
+    </div>
