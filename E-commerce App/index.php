13fd0e16470be28316b77f38807a7d0078308bea
MusharibIqbal <45254288+MusharibIqbal@users.noreply.github.com>
2019-04-29 21:07:17 +0500

    Create index.php

---
+++ E-commerce App/index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>E-SHOP HTML Template</title>
+
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+
+    <!-- Slick -->
+    <link type="text/css" rel="stylesheet" href="css/slick.css" />
+    <link type="text/css" rel="stylesheet" href="css/slick-theme.css" />
+    <link type="text/css" rel="stylesheet" href="css/nouislider.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
+
+</head>
+
+<body>
+<?php include_once ('header.html');
+    ?>
+    <div id="navigation">
+        <!-- container -->
+        <div class="container">
+            <div id="responsive-nav">
+                    <div class="category-nav ">
+                            <span class="category-header">Categories <i class="fa fa-list"></i></span>
+                            <ul class="category-list">
+                                <li class="dropdown side-dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Men's Clothing <i
+                                            class="fa fa-angle-right"></i></a>
+                                    <div class="custom-menu">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <ul class="list-links">
+                                                    <li>
+                                                        <h3 class="list-links-title">Categories</h3>
+                                                    </li>
+                                                    <li><a href="#">Women’s Clothing</a></li>
+                                                    <li><a href="#">Men’s Clothing</a></li>
+                                                    <li><a href="#">Phones & Accessories</a></li>
+                                                    <li><a href="#">Jewelry & Watches</a></li>
+                                                    <li><a href="#">Bags & Shoes</a></li>
+                                                </ul>
+                                                <hr class="hidden-md hidden-lg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="dropdown side-dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Women’s Clothing <i
+                                            class="fa fa-angle-right"></i></a>
+                                    <div class="custom-menu">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <ul class="list-links">
+                                                    <li>
+                                                        <h3 class="list-links-title">Categories</h3>
+                                                    </li>
+                                                    <li><a href="#">Women’s Clothing</a></li>
+                                                    <li><a href="#">Men’s Clothing</a></li>
+                                                    <li><a href="#">Phones & Accessories</a></li>
+                                                    <li><a href="#">Jewelry & Watches</a></li>
+                                                    <li><a href="#">Bags & Shoes</a></li>
+                                                </ul>
+                                                <hr class="hidden-md hidden-lg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="dropdown side-dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Consumer Electronics
+                                        <i class="fa fa-angle-right"></i></a>
+                                    <div class="custom-menu">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <ul class="list-links">
+                                                    <li>
+                                                        <h3 class="list-links-title">Categories</h3>
+                                                    </li>
+                                                    <li><a href="#">Women’s Clothing</a></li>
+                                                    <li><a href="#">Men’s Clothing</a></li>
+                                                    <li><a href="#">Phones & Accessories</a></li>
+                                                    <li><a href="#">Jewelry & Watches</a></li>
+                                                    <li><a href="#">Bags & Shoes</a></li>
+                                                </ul>
+                                                <hr class="hidden-md hidden-lg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="dropdown side-dropdown">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Home & LifeStyle <i class="fa fa-angle-right"></i></a>
+                                        <div class="custom-menu">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <ul class="list-links">
+                                                        <li>
+                                                            <h3 class="list-links-title">Categories</h3></li>
+                                                        <li><a href="#">Women’s Clothing</a></li>
+                                                        <li><a href="#">Men’s Clothing</a></li>
+                                                        <li><a href="#">Phones & Accessories</a></li>
+                                                        <li><a href="#">Jewelry & Watches</a></li>
+                                                        <li><a href="#">Bags & Shoes</a></li>
+                                                    </ul>
+                                                    <hr class="hidden-md hidden-lg">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                <li class="dropdown side-dropdown">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Sports and Travel<i class="fa fa-angle-right"></i></a>
+                                        <div class="custom-menu">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <ul class="list-links">
+                                                        <li>
+                                                            <h3 class="list-links-title">Categories</h3></li>
+                                                        <li><a href="#">Women’s Clothing</a></li>
+                                                        <li><a href="#">Men’s Clothing</a></li>
+                                                        <li><a href="#">Phones & Accessories</a></li>
+                                                        <li><a href="#">Jewelry & Watches</a></li>
+                                                        <li><a href="#">Bags & Shoes</a></li>
+                                                    </ul>
+                                                    <hr class="hidden-md hidden-lg">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                <li class="dropdown side-dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">Jewelery and Accessories <i class="fa fa-angle-right"></i></a>
+                                    <div class="custom-menu">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <ul class="list-links">
+                                                    <li>
+                                                        <h3 class="list-links-title">Categories</h3>
+                                                    </li>
+                                                    <li><a href="#">Women’s Clothing</a></li>
+                                                    <li><a href="#">Men’s Clothing</a></li>
+                                                    <li><a href="#">Phones & Accessories</a></li>
+                                                    <li><a href="#">Jewelry & Watches</a></li>
+                                                    <li><a href="#">Bags & Shoes</a></li>
+                                                </ul>
+                                                <hr class="hidden-md hidden-lg">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                <!-- menu nav -->
+                <div class="menu-nav">
+                    <span class="menu-header">Menu <i class="fa fa-bars"></i></span>
+                    <ul class="menu-list">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Women</a></li>
+                        <li><a href="#">Sales</a></li>
+                    </ul>
+                </div>
+                <!-- menu nav -->
+            </div>
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /NAVIGATION -->
+    <!-- HOME -->
+    <div id="home">
+        <!-- container -->
+        <div class="container">
+            <!-- home wrap -->
+            <div class="home-wrap">
+                <!-- home slick -->
+                <div id="home-slick">
+                    <!-- banner -->
+                    <div class="banner banner-1">
+                        <img src="./img/banner01.jpg" alt="" height="300px">
+                        <div class="banner-caption text-center">
+                            <h1>Bags sale</h1>
+                            <h3 class="white-color font-weak">Up to 50% Discount</h3>
+                            <button class="primary-btn">Shop Now</button>
+                        </div>
+                    </div>
+                    <!-- /banner -->
+                    <!-- banner -->
+                    <div class="banner banner-1">
+                        <img src="./img/banner02.jpg" alt="">
+                        <div class="banner-caption">
+                            <h1 class="primary-color">HOT DEAL<br><span class="white-color font-weak">Up to 50% OFF</span></h1>
+                            <button class="primary-btn">Shop Now</button>
+                        </div>
+                    </div>
+                    <!-- /banner -->
+                    <!-- banner -->
+                    <div class="banner banner-1">
+                        <img src="./img/banner03.jpg" alt="">
+                        <div class="banner-caption">
+                            <h1 class="white-color">New Product <span>Collection</span></h1>
+                            <button class="primary-btn">Shop Now</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+    <div class="section">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- section title -->
+                <div class="col-md-12">
+                    <div class="section-title">
+                        <h2 class="title">Laptops</h2>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="product product-single">
+                        <div class="product-thumb">
+                            <img src="./img/product01.jpg" alt="">
+                        </div>
+                        <div class="product-body">
+                            <h3 class="product-price">Rs. 1000</h3>
+                            <div class="product-rating">
+                                <p> 4.5 </p>
+                                <i class="fa fa-star"></i>
+
+                            </div>
+                            <h2 class="product-name"><a href="#">Hp111</a></h2>
+                            <div class="product-name">Brand HP</div>
+                            
+                        </div>
+                    </div>
+                </div>
+               
+                <div class="col-md-3">
+                    <div class="product product-single">
+                        <div class="product-thumb">
+                            
+                            <img src="./img/product02.jpg" alt="">
+                        </div>
+                        <div class="product-body">
+                            <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                            <div class="product-rating">
+                                <i class="fa fa-star"></i>
+                                <p> 4.5 </p>
+                            </div>
+                            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            <div class="product-btns">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="product product-single">
+                        <div class="product-thumb">
+                            <div class="product-label">
+                            
+                            </div>
+                            
+                            <img src="./img/product03.jpg" alt="">
+                        </div>
+                        <div class="product-body">
+                            <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                            <div class="product-rating">
+                                <i class="fa fa-star"></i>
+                                <p> 4.5 </p>
+                            </div>
+                            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            <div class="product-btns">
+                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 ">
+                    <div class="product product-single">
+                        <div class="product-thumb">
+                            <div class="product-label">
+                                <span>New</span>
+                            </div>
+                           
+                            <img src="./img/product04.jpg" alt="">
+                        </div>
+                        <div class="product-body">
+                            <h3 class="product-price">$32.50</h3>
+                            <div class="product-rating">
+                                <i class="fa fa-star"></i>
+                                <p> 4.5 </p>
+                            </div>
+                            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                            <div class="product-btns">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    
+<?php
+include_once('footer.html');
+?>
+</body>
+
+</html>
